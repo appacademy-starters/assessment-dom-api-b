92192bc8efca0fedda560acf5d12c3467b1855ea
junnac <bigdogstore@gmail.com>
2020-07-10 12:21:13 -0700

    Add starter assessment

---
+++ event-handling-project.js
@@ -0,0 +1,28 @@
+// Make sure the DOM is ready
+
+  // Phase 1: Counting div clicks
+
+
+
+  // Phase 2: Field value event
+
+
+
+  // Phase 3: Focus event
+
+
+
+  // Phase 4: Checking different emails
+
+
+
+  // Phase 5: Checkbox event
+
+
+
+  // Phase 6: Print event target to console
+
+
+
+// Phase 7: Drag-and-drop
+
